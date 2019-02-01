7b96aa2e206ef9104d5368571f1ca63aa574f0ea
kaisenaiko <kaisenaiko@gmail.com>
2019-02-02 02:13:27 +0700

    instafeed

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '1291858806',
+        limit: 12,
+        resolution: 'standard_resolution',
+        accessToken: '1291858806.1677ed0.fd57ac45145b4c7abe62eaae70fb3d2b',
+        sortBy: 'most-recent',
+        template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+    });
+
+
+    userFeed.run();
+
+    
+    // This will create a single gallery from all elements that have class "gallery-item"
+    $('.gallery').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+});
